5c12828fbe80c87f4e23549135a483a417ab2dd2
Tom N Harris <tnharris@whoopdedo.org>
2013-07-17 04:35:58 -0400

    Methods for making cScrStr safe to use as an output parameter.

--- lg/types.h
+++ lg/types.h
@@ -208,6 +208,7 @@ public:
 		{ }
 	cScrStr (const char* psz)
 		{ m_pszData = IF_NOT(psz, _ChNil); }
+	cScrStr (uint sz);
 
 	cScrStr& operator = (const char* psz)
 		{ m_pszData = IF_NOT(psz, _ChNil); return *this;}
@@ -220,6 +221,10 @@ public:
 	// No checking is performed, so be absolutely sure when you call this.
 	void Free();
 	void Copy(const char* psz);
+	// Set the string pointer to NULL before using it as
+	// an output parameter.
+	void MakeNull()
+		{ m_pszData = NULL; }
 
 	Bool IsEmpty(void) const
 		{ return *m_pszData == 0; }
